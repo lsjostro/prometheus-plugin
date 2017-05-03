343f4695f41bedb1fe84bc8770c9f495513d1570
Robin Müller <coder-hugo@users.noreply.github.com>
2017-05-03 17:11:59 +0200

    Add possibility to configure the prometheus endpoint in the global settings and allow slashes within the path

---
+++ src/main/java/org/jenkinsci/plugins/prometheus/config/PrometheusConfiguration.java
@@ -0,0 +1,76 @@
+package org.jenkinsci.plugins.prometheus.config;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+import jenkins.model.GlobalConfiguration;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Robin Müller
+ */
+@Extension
+public class PrometheusConfiguration extends GlobalConfiguration {
+
+    private static final String PROMETHEUS_ENDPOINT = "PROMETHEUS_ENDPOINT";
+    private static final String DEFAULT_ENDPOINT = "prometheus";
+
+    private String urlName;
+    private String additionalPath;
+
+    public PrometheusConfiguration() {
+        load();
+        if (urlName == null) {
+            Map<String, String> env = System.getenv();
+            setPath(env.containsKey(PROMETHEUS_ENDPOINT) ? env.get(PROMETHEUS_ENDPOINT) : DEFAULT_ENDPOINT);
+        }
+    }
+
+    public static PrometheusConfiguration get() {
+        Descriptor configuration = Jenkins.getActiveInstance().getDescriptor(PrometheusConfiguration.class);
+        return (PrometheusConfiguration) configuration;
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        setPath(json.getString("path"));
+        save();
+        return super.configure(req, json);
+    }
+
+    public void setPath(String path) {
+        urlName = path.split("/")[0];
+        List<String> pathParts = Arrays.asList(path.split("/"));
+        additionalPath = (pathParts.size() > 1 ? "/" : "") + StringUtils.join(pathParts.subList(1, pathParts.size()), "/");
+    }
+
+    public String getPath() {
+        return StringUtils.isEmpty(additionalPath) ? urlName : urlName + "/" + additionalPath;
+    }
+
+    public String getUrlName() {
+        return urlName;
+    }
+
+    public String getAdditionalPath() {
+        return additionalPath;
+    }
+
+    public FormValidation doCheckPath(@QueryParameter String value) {
+        if (StringUtils.isEmpty(value)) {
+            return FormValidation.error(Messages.path_required());
+        } else if (System.getenv().containsKey(PROMETHEUS_ENDPOINT)) {
+            return FormValidation.warning(Messages.path_environment_override(PROMETHEUS_ENDPOINT, System.getenv(PROMETHEUS_ENDPOINT)));
+        } else {
+            return FormValidation.ok();
+        }
+    }
+}
--- src/main/java/org/jenkinsci/plugins/prometheus/rest/PrometheusAction.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/rest/PrometheusAction.java
@@ -1,19 +1,21 @@
 package org.jenkinsci.plugins.prometheus.rest;
 
 import hudson.Extension;
+import hudson.model.RootAction;
 import hudson.model.UnprotectedRootAction;
+import hudson.util.HttpResponses;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.dropwizard.DropwizardExports;
 import jenkins.metrics.api.Metrics;
 import org.jenkinsci.plugins.prometheus.JobCollector;
 import org.jenkinsci.plugins.prometheus.MetricsRequest;
+import org.jenkinsci.plugins.prometheus.config.PrometheusConfiguration;
+import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public class PrometheusAction implements UnprotectedRootAction {
     private CollectorRegistry collectorRegistry;
     private JobCollector jobCollector = new JobCollector();
-    private static final String DEFAULT_ENDPOINT = "prometheus";
-    private String prometheusEndpoint;
 
     @Override
     public String getIconFileName() {
@@ -27,21 +29,20 @@ public class PrometheusAction implements UnprotectedRootAction {
 
     @Override
     public String getUrlName() {
-        prometheusEndpoint = System.getenv("PROMETHEUS_ENDPOINT");
-        if (prometheusEndpoint == null || prometheusEndpoint.length() == 0) {
-            prometheusEndpoint = DEFAULT_ENDPOINT;
-        }
-        return prometheusEndpoint;
+        return PrometheusConfiguration.get().getUrlName();
     }
 
-    public Object doIndex() {
-        if (collectorRegistry == null) {
-            collectorRegistry = CollectorRegistry.defaultRegistry;
-            collectorRegistry.register(jobCollector);
-            if (Metrics.metricRegistry() != null) {
-                collectorRegistry.register(new DropwizardExports(Metrics.metricRegistry()));
+    public Object doDynamic(StaplerRequest request) {
+        if (request.getRestOfPath().equals(PrometheusConfiguration.get().getAdditionalPath())) {
+            if (collectorRegistry == null) {
+                collectorRegistry = CollectorRegistry.defaultRegistry;
+                collectorRegistry.register(jobCollector);
+                if (Metrics.metricRegistry() != null) {
+                    collectorRegistry.register(new DropwizardExports(Metrics.metricRegistry()));
+                }
             }
+            return MetricsRequest.prometheusResponse(collectorRegistry);
         }
-        return MetricsRequest.prometheusResponse(collectorRegistry);
+        throw HttpResponses.notFound();
     }
 }
