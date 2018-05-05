4520ae205b688d544c57d34d4ff452870e8d13a0
c5403 <james.p.mcshane@healthpartners.com>
2018-05-05 09:46:33 -0500

    Adding null checking

--- src/main/java/org/jenkinsci/plugins/prometheus/JenkinsStatusCollector.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/JenkinsStatusCollector.java
@@ -1,6 +1,6 @@
 package org.jenkinsci.plugins.prometheus;
 
-import hudson.model.AdministrativeMonitor;
+import hudson.model.Computer;
 import io.prometheus.client.Collector;
 import io.prometheus.client.Gauge;
 import jenkins.model.Jenkins;
@@ -11,32 +11,34 @@ import java.util.List;
 
 public class JenkinsStatusCollector extends Collector {
 
-    private Gauge jenkinsUp;
-    private Gauge jenkinsUptime;
-
     @Override
     public List<MetricFamilySamples> collect() {
         final String subsystem = "jenkins";
         final String namespace = ConfigurationUtils.getNamespace();
         final List<MetricFamilySamples> samples = new ArrayList<>();
-
-        long upTime = Jenkins.getInstance().toComputer().getConnectTime();
-        System.out.println(upTime);
-        jenkinsUp = Gauge.build().
+        Gauge jenkinsUp = Gauge.build().
                 name("up").
-                subsystem(subsystem).namespace(namespace).
+                labelNames().
+                subsystem(subsystem).
+                namespace(namespace).
                 help("Is Jenkins ready to receive requests").
                 create();
         jenkinsUp.set(Jenkins.getInstance().getInitLevel() == hudson.init.InitMilestone.COMPLETED ?
                 1 : 0);
         samples.addAll(jenkinsUp.collect());
-        jenkinsUptime = Gauge.build().
+        Gauge jenkinsUptime = Gauge.build().
                 name("uptime").
-                subsystem(subsystem).namespace(namespace).
+                labelNames().
+                subsystem(subsystem).
+                namespace(namespace).
                 help("Time since Jenkins machine was initialized").
                 create();
-        jenkinsUptime.set(System.currentTimeMillis() - upTime);
-        samples.addAll(jenkinsUptime.collect());
+        Computer computer = Jenkins.getInstance().toComputer();
+        if (computer != null) {
+            long upTime = computer.getConnectTime();
+            jenkinsUptime.set(System.currentTimeMillis() - upTime);
+            samples.addAll(jenkinsUptime.collect());
+        }
         return samples;
     }
 }
