fc3d0d9b665c0b3b26e0a3d6f8380e419508cff8
markyjackson-taulia <marky.r.jackson@gmail.com>
2022-05-24 13:44:08 -0700

    FIxing up bad class

--- src/test/java/org/jenkinsci/plugins/prometheus/util/MetricsFormatterTest.java
+++ src/test/java/org/jenkinsci/plugins/prometheus/util/MetricsFormatterTest.java
@@ -17,22 +17,6 @@ public class MetricsFormatterTest {
             "jenkins_node_my_node_1_builds_count 12345";
 
     @Test
-    public void master_node_count_format() {
-        String formatString = MetricsFormatter.formatMetrics(inString);
-        //master node count
-        Assert.assertFalse(formatString.contains("jenkins_node_builds_count 28458.0\n"));
-        Assert.assertTrue(formatString.contains("jenkins_node_builds_count{node=\"master\"} 28458.0\n"));
-    }
-
-    @Test
-    public void master_node_histogram_format() {
-        String formatString = MetricsFormatter.formatMetrics(inString);
-        //master node histogram
-        Assert.assertFalse(formatString.contains("jenkins_node_builds{quantile=\"0.999\",} 0.091670452\n"));
-        Assert.assertTrue(formatString.contains("jenkins_node_builds{node=\"master\", quantile=\"0.999\",} 0.091670452\n"));
-    }
-
-    @Test
     public void named_node_count_format() {
         String formatString = MetricsFormatter.formatMetrics(inString);
         //named node count
