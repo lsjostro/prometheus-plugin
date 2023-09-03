c1efb91ee217fd83c6b5f03d0b102c4e6a1a3a83
waschndolos <killear@gmx.de>
2023-09-03 13:00:32 +0200

    Fixing optionalProperty handling

--- src/main/java/org/jenkinsci/plugins/prometheus/config/PrometheusConfiguration.java
+++ src/main/java/org/jenkinsci/plugins/prometheus/config/PrometheusConfiguration.java
@@ -14,6 +14,7 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.CheckForNull;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -59,7 +60,8 @@ public class PrometheusConfiguration extends GlobalConfiguration {
     private boolean collectCodeCoverage = false;
     private boolean collectNodeStatus = true;
 
-    private DisabledMetricConfig disabledMetricConfig = new DisabledMetricConfig(new ArrayList<>());
+    @CheckForNull
+    private DisabledMetricConfig disabledMetricConfig;
 
 
     public PrometheusConfiguration() {
@@ -76,6 +78,7 @@ public class PrometheusConfiguration extends GlobalConfiguration {
 
     @Override
     public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        disabledMetricConfig = null;
         req.bindJSON(this, json);
         save();
         return true;
